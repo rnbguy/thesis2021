716393ff0d26481fe5aff879b91ae953421b7e02
Ranadeep Biswas <ranadip.bswas@gmail.com>
2021-01-07 01:34:47 +0100

    updates

--- cite.sh
+++ cite.sh
@@ -3,4 +3,8 @@
 PRE=$1
 AFT=$2
 
-find Sources -iname '*tex' | xargs grep -il "\cite{${PRE}}" | xargs sed -i "s|\\\\cite{${PRE}}|\\\\cite{DBLP:${AFT}}|"
+find Sources -iname '*tex' | xargs grep -il "${PRE}" | xargs sed -i "s|${PRE}|DBLP:${AFT}|"
+
+# for e in `find Sources/ -iname '*tex' -exec egrep -o '\\cite[tp]?{[^}]*}' {} \; | cut -d '{' -f 2 | cut -d '}' -f 1 | tr -d ' ' | tr , '\n' | sort | uniq | grep -v DBLP`; do
+#     grep -inr $e Sources/*/misc.bib > /dev/null || echo $e
+# done
